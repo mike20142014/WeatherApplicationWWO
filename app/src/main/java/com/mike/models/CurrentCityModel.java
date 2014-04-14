6c42e6b629b43588e959a70a39a8c208fd385859
mike20132013 <michael.tirkey2012@yahoo.com>
2014-04-14 16:04:07 -0400

    Added model for country

--- app/src/main/java/com/mike/models/CurrentCityModel.java
+++ app/src/main/java/com/mike/models/CurrentCityModel.java
@@ -8,12 +8,21 @@ public class CurrentCityModel {
 
     String currentCity;
 
+    String currentCountry;
 
     public CurrentCityModel() {
     }
 
     public CurrentCityModel(String currentCity) {
         this.currentCity = currentCity;
+    }
+
+    public String getCurrentCountry() {
+        return currentCountry;
+    }
+
+    public void setCurrentCountry(String currentCountry) {
+        this.currentCountry = currentCountry;
     }
 
     public String getCurrentCity() {
